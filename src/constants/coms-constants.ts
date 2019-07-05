b5b506df7368de700dc3cc5a200aa5635b1665b9
Pranjali <rathee.pranjali@cris.org.in>
2019-07-05 17:44:09 +0530

    change

--- src/constants/coms-constants.ts
+++ src/constants/coms-constants.ts
@@ -1,8 +1,8 @@
 export const CONTEXT_ROOT              = 'madad/api/';
 //export const BASE_URL                  = 'http://10.64.15.80:8080/';
-/*
-production export const BASE_URL                  = 'http://203.176.113.183/';
 
-*/
+ export const BASE_URL                  = 'http://203.176.113.183/';
 
-export const BASE_URL                  = 'http://localhost:8080/';
+
+
+//export const BASE_URL                  = 'http://localhost:8080/';
