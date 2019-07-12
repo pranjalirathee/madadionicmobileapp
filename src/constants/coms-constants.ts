ab661e99d891b58782525ecfc27ea845b83e14ea
Pranjali <rathee.pranjali@cris.org.in>
2019-07-12 17:24:32 +0530

    upd

--- src/constants/coms-constants.ts
+++ src/constants/coms-constants.ts
@@ -1,7 +1,7 @@
 export const CONTEXT_ROOT              = 'madad/api/';
 //export const BASE_URL                  = 'http://10.64.15.80:8080/';
 
- export const BASE_URL                  = 'http://203.176.113.183/';
+ export const BASE_URL                  = 'http://www.madad.indianrailways.gov.in/';
 
 
 
